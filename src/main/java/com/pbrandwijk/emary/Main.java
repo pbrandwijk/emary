210b0d0e1c07410e7de2400481b303576f915bc5
Pieter Brandwijk <pieter@pbrandwijk.com>
2018-10-27 20:47:13 +0200

    #1 Update Main class stub with LOGGER

--- src/main/java/com/pbrandwijk/emary/Main.java
+++ src/main/java/com/pbrandwijk/emary/Main.java
@@ -1,9 +1,14 @@
 package com.pbrandwijk.emary;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello from Emary");
+        LOGGER.info("Hello from Emary");
     }
 
 }
