127230c115ebf3c715e61f5f13bdf942dceb2b3e
Pieter Brandwijk <pieter@pbrandwijk.com>
2018-10-28 18:46:28 +0100

    #2 Update main method to call the XSLT transformer

--- src/main/java/com/pbrandwijk/emary/Main.java
+++ src/main/java/com/pbrandwijk/emary/Main.java
@@ -3,12 +3,21 @@ package com.pbrandwijk.emary;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
-        LOGGER.info("Hello from Emary");
+
+        try {
+            Transformer.transform(new FileInputStream(new File(args[0])));
+        } catch (FileNotFoundException e) {
+            LOGGER.warn("XML file could not be found", e);
+        }
     }
 
 }
